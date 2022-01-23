4570be77055fb4397e8b7b2286647d0cd03a8ad6
Alexander <aroman.link11@gmail.com>
2022-01-23 00:01:33 -0500

    segundo commit

--- q_tutorial/spiders/quot.py
+++ q_tutorial/spiders/quot.py
@@ -2,7 +2,7 @@ import scrapy
 
 #Título = '//h1/a/text()'
 #Citas = '//span[@class = "text" and  @itemprop = "text"]/text()'
-#Citas top = '//span[@class = "tag-item"]/a[@class = "tag"]/text()'
+#Citas top = '//div[contains(@class, "tags-box")]//span[@class = "tag-item"]/a/text()'
 #Next page button = '//ul[@class = "pager"]//li[@class = "next"]/a/@href'
 
 class QuotesSpider(scrapy.Spider):
@@ -17,12 +17,36 @@ class QuotesSpider(scrapy.Spider):
             'quotes.json': {
                 'format': 'json',
                 'encoding': 'utf8',
-                'fields': ['title', 'quotes', 'top_ten_tags'],
+                'fields': ['title', 'tags_top', 'quotes'], #Estos campos tienen que estar igual que las variables
                 'overwrite': True
             }
         }
+
+        ,
+
+        'MEMUSAGE_LIMIT_MB' : 2048 #Para decirle al spyder cuanta memoria ram utilizar
     }
 
+    def parse_only_quotes(self, response, **kwargs): #guardar las citas
+
+        if kwargs:
+
+            quotes = kwargs['quotes']
+
+        quotes.extend(response.xpath('//span[@class = "text" and  @itemprop = "text"]/text()').getall())
+
+        next_page_button_link = response.xpath('//ul[@class = "pager"]//li[@class = "next"]/a/@href').get()
+
+        if next_page_button_link:
+
+            yield response.follow(next_page_button_link, callback = self.parse_only_quotes, cb_kwargs = {'quotes': quotes})
+        
+        else:
+
+            yield{
+
+                'quotes' : quotes
+            }
 
     def parse(self, response): #Analizar un archivo para extraer información valiosa
 
@@ -30,11 +54,17 @@ class QuotesSpider(scrapy.Spider):
         
         quotes = response.xpath('//span[@class = "text" and  @itemprop = "text"]/text()').getall()
         
-        tags_top = response.xpath('//span[@class = "tag-item"]/a[@class = "tag"]/text()').getall()
+        tags_top = response.xpath('//div[@class="col-md-4 tags-box"]//span[@class="tag-item"]/a/text()').getall()
         
+        top = getattr(self, 'top', None) #Argumento para sacar el top 5 o lo que queramos de tags_top
+        
+        if top:
+
+            top = int(top)
+            tags_top = tags_top[:top] #Nos traer hasta el indice que queremos
+            
         yield{
             'title' : title,
-            'quotes' : quotes,
             'tags_top' : tags_top
 
         }
@@ -44,6 +74,6 @@ class QuotesSpider(scrapy.Spider):
 
         if next_page_button_link:
 
-            yield response.follow(next_page_button_link, callback = self.parse)
+            yield response.follow(next_page_button_link, callback = self.parse_only_quotes, cb_kwargs = {'quotes': quotes})
         
-        
+      
